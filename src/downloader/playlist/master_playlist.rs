a65652895f1838d64c7a47dee20d5ff77ea06ba2
Suleimen Zhonissov <zhonisov.s@gmail.com>
2025-06-17 13:23:58 +0500

    version upd (downloading videos without audio)

--- src/downloader/playlist/master_playlist.rs
+++ src/downloader/playlist/master_playlist.rs
@@ -1,60 +1,70 @@
+use crate::downloader::{downloader_error::DownloaderError, playlist::media_playlist::MediaPlaylist};
 use tokio::process::Command;
-use tracing::info;
-
-use super::media_playlist::MediaPlaylist;
-use crate::downloader::downloader_error::DownloaderError;
 
 pub struct MasterPlaylist {
   pub resolution: String,
   video_media_playlist: Option<MediaPlaylist>,
   audio_media_playlist: Option<MediaPlaylist>,
   video_media_url: String,
-  audio_media_url: String,
+  audio_media_url: Option<String>
 }
 
 impl MasterPlaylist {
-  pub async fn from_urls(video_url: &str, audio_url: &str) -> Result<Self, DownloaderError> {
+  pub async fn from_urls(video_url: String, audio_url: Option<String>) -> Result<Self, DownloaderError> {
     Ok(MasterPlaylist {
       resolution: String::new(),
       video_media_playlist: None,
       audio_media_playlist: None,
-      video_media_url: video_url.to_string(),
-      audio_media_url: audio_url.to_string(),
+      video_media_url: video_url,
+      audio_media_url: audio_url
     })
   }
 
   pub async fn download(&mut self) -> Result<String, DownloaderError> {
-    info!("Downloading video with urls: {} and {}", self.video_media_url, self.audio_media_url);
-
     self.video_media_playlist = Some(MediaPlaylist::from_url(&self.video_media_url).await?);
-    self.audio_media_playlist = Some(MediaPlaylist::from_url(&self.audio_media_url).await?);
     let video_media_playlist = self.video_media_playlist.as_ref().unwrap();
-    let audio_media_playlist = self.audio_media_playlist.as_ref().unwrap();
-
     let video_bytes = video_media_playlist.get_byte_data();
-    let audio_bytes = audio_media_playlist.get_byte_data();
 
     let video_name = video_media_playlist.name.split('/').last().unwrap().split('.').next().unwrap().to_string();
-    let audio_name = audio_media_playlist.name.split('/').last().unwrap().split('.').next().unwrap().to_string();
     let output_name = format!("{}_{}.mp4", video_name, self.resolution);
 
     tokio::fs::write(video_name.clone(), video_bytes).await.map_err(|_| DownloaderError::IOError)?;
-    tokio::fs::write(audio_name.clone(), audio_bytes).await.map_err(|_| DownloaderError::IOError)?;
 
-    Command::new("ffmpeg")
-      .args(&["-i", &video_name])
-      .args(&["-i", &audio_name])
-      .args(["-c", "copy"])
-      .arg("-y")
-      .arg(&output_name)
-      .output()
-      .await
-      .map_err(|_| DownloaderError::FfmpegError)?;
+    match &self.audio_media_url {
+      Some(audio_media_url) => {
+        self.audio_media_playlist = Some(MediaPlaylist::from_url(&audio_media_url).await?);
+        let audio_media_playlist = self.audio_media_playlist.as_ref().unwrap();
+        let audio_bytes = audio_media_playlist.get_byte_data();
+
+        let audio_name = audio_media_playlist.name.split('/').last().unwrap().split('.').next().unwrap().to_string();
+        tokio::fs::write(audio_name.clone(), audio_bytes).await.map_err(|_| DownloaderError::IOError)?;
+
+        Command::new("ffmpeg")
+          .args(&["-i", &video_name])
+          .args(&["-i", &audio_name])
+          .args(["-c", "copy"])
+          .arg("-y")
+          .arg(&output_name)
+          .output()
+          .await
+          .map_err(|_| DownloaderError::FfmpegError)?;
+
+        tokio::fs::remove_file(audio_name).await.map_err(|_| DownloaderError::IOError)?;
+      }
+      None => {
+        Command::new("ffmpeg")
+          .args(&["-i", &video_name])
+          .args(["-c", "copy"])
+          .arg("-y")
+          .arg(&output_name)
+          .output()
+          .await
+          .map_err(|_| DownloaderError::FfmpegError)?;
+      }
+    }
 
     tokio::fs::remove_file(video_name).await.map_err(|_| DownloaderError::IOError)?;
-    tokio::fs::remove_file(audio_name).await.map_err(|_| DownloaderError::IOError)?;
 
-    info!("Downloaded video {} successfully", output_name);
     Ok(output_name)
   }
 }
--- src/downloader/playlist/variant_playlist.rs
+++ src/downloader/playlist/variant_playlist.rs
@@ -2,8 +2,7 @@ use futures::future::join_all;
 use regex::Regex;
 use std::collections::HashMap;
 
-use super::master_playlist::MasterPlaylist;
-use crate::downloader::downloader_error::DownloaderError;
+use crate::downloader::{downloader_error::DownloaderError, playlist::master_playlist::MasterPlaylist};
 
 pub struct VariantPlaylist {
   pub master_playlists: Vec<MasterPlaylist>,
@@ -12,74 +11,56 @@ pub struct VariantPlaylist {
 impl VariantPlaylist {
   pub async fn from_url(url: &str) -> Result<Self, DownloaderError> {
     const BASE_URL: &str = "https://video.twimg.com";
-    struct ParsedOutput {
-      video_url: Option<String>,
-      audio_url: Option<String>,
-      resolution: Option<String>,
-    }
-
-    let mut output_map = HashMap::<String, ParsedOutput>::new();
 
     let response =
       reqwest::get(url).await.map_err(|_| DownloaderError::FetchError)?.text().await.map_err(|_| DownloaderError::FetchError)?;
 
-    let lines = response.lines().filter(|&line| !line.is_empty()).collect::<Vec<&str>>();
+    //audio pass
+    const AUDIO_REGEX_STR: &str = r#"(?m)^#EXT-X-MEDIA:NAME="Audio".*GROUP-ID="(?P<audio_tag>audio-\d*)".*URI="(?P<audio_url>.*)""#;
 
-    const AUDIO_REGEX_STR: &str = r#"^#EXT-X-MEDIA:NAME="Audio".*GROUP-ID="(?P<audio_tag>.*)".*URI="(?P<audio_media_url>.*)"$"#;
-    const VIDEO_REGEX_STR: &str = r#"^#EXT-X-STREAM-INF.*RESOLUTION=(?P<video_resolution>\d*x\d*),.*AUDIO="(?P<audio_match_tag>.*)"$"#;
-    let final_regex_string = format!("{AUDIO_REGEX_STR}|{VIDEO_REGEX_STR}");
-
-    let regex = Regex::new(&final_regex_string).unwrap();
-    for (i, line) in lines.iter().enumerate() {
-      match regex.captures(line) {
-        Some(audio_captures) if audio_captures.name("audio_media_url").is_some() => {
-          let mut audio_media_url = audio_captures.name("audio_media_url").unwrap().as_str().to_string();
-          audio_media_url = format!("{BASE_URL}{audio_media_url}");
-
-          if let Some(audio_tag) = audio_captures.name("audio_tag").map(|tag| tag.as_str().to_string()) {
-            output_map.entry(audio_tag).and_modify(|o| o.audio_url = Some(audio_media_url.clone())).or_insert(ParsedOutput {
-              video_url: None,
-              audio_url: Some(audio_media_url.clone()),
-              resolution: None,
-            });
-          }
-        }
-        Some(video_captures) if video_captures.name("audio_match_tag").is_some() => {
-          let audio_match_tag = video_captures.name("audio_match_tag").unwrap().as_str().to_string();
-          let mut video_media_url = lines[i + 1].to_string();
-          video_media_url = format!("{BASE_URL}{video_media_url}");
-
-          if let Some(resolution) = video_captures.name("video_resolution").map(|res| res.as_str().to_string()) {
-            output_map
-              .entry(audio_match_tag)
-              .and_modify(|o| {
-                o.video_url = Some(video_media_url.clone());
-                o.resolution = Some(resolution.clone());
-              })
-              .or_insert(ParsedOutput {
-                video_url: Some(video_media_url.clone()),
-                audio_url: None,
-                resolution: Some(resolution.clone()),
-              });
-          }
-        }
-        _ => {}
-      }
+    let mut audio_map = HashMap::<String, String>::new();
+    let audio_pass = Regex::new(&AUDIO_REGEX_STR).unwrap();
+    for capture in audio_pass.captures_iter(&response) {
+      let Some(tag) = capture.name("audio_tag") else {
+        continue;
+      };
+      let Some(url) = capture.name("audio_url") else {
+        continue;
+      };
+      audio_map.insert(tag.as_str().to_string(), url.as_str().to_string());
     }
 
-    let mut tasks = vec![];
-    for output in output_map.values() {
-      if output.video_url.is_none() || output.audio_url.is_none() || output.resolution.is_none() {
-        continue;
-      }
+    //video pass
+    let contains_audio = audio_map.len() > 0;
+    let video_regex_str: &str = if contains_audio {
+      r#"(?m)^#EXT-X-STREAM-INF.*RESOLUTION=(?P<video_resolution>\d*x\d*).*AUDIO="(?P<audio_match_tag>audio-\d*)"\n(?P<video_url>.*)"#
+    } else {
+      r#"(?m)^#EXT-X-STREAM-INF.*RESOLUTION=(?P<video_resolution>\d*x\d*).*\n(?P<video_url>.*)"#
+    };
 
-      let video_url = output.video_url.clone().unwrap();
-      let audio_url = output.audio_url.clone().unwrap();
-      let resolution = output.resolution.clone().unwrap();
+    let mut tasks = vec![];
+    let video_pass = Regex::new(video_regex_str).unwrap();
+    for capture in video_pass.captures_iter(&response) {
+      let Some(resolution) = capture.name("video_resolution") else {
+        continue;
+      };
+      let Some(video_url) = capture.name("video_url") else {
+        continue;
+      };
+      let audio_url = if contains_audio {
+        capture.name("audio_match_tag").and_then(|tag| audio_map.get(tag.as_str()).cloned())
+      } else {
+        None
+      };
+
+      let full_video_url = format!("{BASE_URL}{}", video_url.as_str());
+      let full_audio_url = audio_url.map(|url| format!("{BASE_URL}{}", url));
+      let resolution_string = resolution.as_str().to_string();
+
       tasks.push(tokio::spawn(async move {
-        match MasterPlaylist::from_urls(&video_url, &audio_url).await {
+        match MasterPlaylist::from_urls(full_video_url, full_audio_url).await {
           Ok(mut master_playlist) => {
-            master_playlist.resolution = resolution;
+            master_playlist.resolution = resolution_string;
             Ok(master_playlist)
           }
           Err(e) => {
@@ -89,6 +70,7 @@ impl VariantPlaylist {
         }
       }));
     }
+
     let results = join_all(tasks).await;
     let mut master_playlists: Vec<MasterPlaylist> = vec![];
 
