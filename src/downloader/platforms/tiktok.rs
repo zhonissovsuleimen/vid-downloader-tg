849dbdfc22b120f52ef3e3544db3f413cec49b98
Suleimen Zhonissov <zhonisov.s@gmail.com>
2026-02-06 23:53:23 +0500

    cargo lock & version bump

--- src/downloader/platforms/tiktok.rs
+++ src/downloader/platforms/tiktok.rs
@@ -1,136 +1,165 @@
 use headless_chrome::{
-  browser::{
-    tab::{RequestInterceptor, RequestPausedDecision},
-    transport::{SessionId, Transport},
-  },
-  protocol::cdp::{
-    Fetch::{events::RequestPausedEvent, RequestPattern, RequestStage},
-    Network::ResourceType,
-    Target::CreateTarget,
-  },
-  Browser,
+    browser::{
+        tab::{RequestInterceptor, RequestPausedDecision},
+        transport::{SessionId, Transport},
+    },
+    protocol::cdp::{
+        Fetch::{events::RequestPausedEvent, RequestPattern, RequestStage},
+        Network::ResourceType,
+        Target::CreateTarget,
+    },
+    Browser,
 };
 use std::sync::Arc;
 use std::time::Duration;
 use tokio::sync::Mutex;
 
 use crate::downloader::{
-  downloader::PlatformDownloader, downloader_error::DownloaderError, playlist::variant_playlist::VariantPlaylist,
+    downloader::PlatformDownloader, downloader_error::DownloaderError,
+    playlist::variant_playlist::VariantPlaylist,
 };
 
 pub struct TiktokDownloader {}
 
 impl PlatformDownloader for TiktokDownloader {
-  async fn download(browser: &Browser, url: &str) -> Result<String, DownloaderError> {
-    let target = get_initial_tab_create_target();
-    let tab = browser.new_tab_with_options(target)?;
-    let intercepted_url = Arc::new(Mutex::new(String::new()));
-    let intercepted_cookie = Arc::new(Mutex::new(String::new()));
-    let interceptor = get_interceptor(intercepted_url.clone(), intercepted_cookie.clone());
+    async fn download(browser: &Browser, url: &str) -> Result<String, DownloaderError> {
+        let target = get_initial_tab_create_target();
+        let tab = browser.new_tab_with_options(target)?;
+        let intercepted_url = Arc::new(Mutex::new(String::new()));
+        let intercepted_cookie = Arc::new(Mutex::new(String::new()));
+        let interceptor = get_interceptor(intercepted_url.clone(), intercepted_cookie.clone());
 
-    tab.enable_fetch(Some(&get_request_patterns()), None)?;
-    tab.enable_request_interception(interceptor)?;
-    tab.navigate_to(url)?;
+        tab.enable_fetch(Some(&get_request_patterns()), None)?;
+        tab.enable_request_interception(interceptor)?;
+        tab.navigate_to(url)?;
 
-    let mut found = false;
-    let mut timeout = 10.0 as f32;
-    while !found && timeout >= 0.0 {
-      found = !intercepted_url.lock().await.is_empty();
-      tokio::time::sleep(Duration::from_millis(100)).await;
-      timeout -= 0.1;
+        let mut found = false;
+        let mut timeout = 10.0 as f32;
+        while !found && timeout >= 0.0 {
+            found = !intercepted_url.lock().await.is_empty();
+            tokio::time::sleep(Duration::from_millis(100)).await;
+            timeout -= 0.1;
+        }
+        let _ = tab.close(false);
+
+        if !found {
+            return Err(DownloaderError::FetchError);
+        }
+
+        let client = reqwest::Client::new();
+
+        let url_mutex_guard = intercepted_url.lock().await.to_owned();
+        let cookie_mutex_guard = intercepted_cookie.lock().await.to_owned();
+        let response = client
+            .get(url_mutex_guard)
+            .header(
+                "User-Agent",
+                r"Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:133.0) Gecko/20100101 Firefox/133.0",
+            )
+            .header("Referer", r"https://www.tiktok.com/")
+            .header("Cookie", cookie_mutex_guard)
+            .send()
+            .await
+            .map_err(|_| DownloaderError::FetchError)?;
+
+        let bytes = response
+            .bytes()
+            .await
+            .map_err(|_| DownloaderError::FetchError)?;
+
+        let mut output_name = url
+            .split('/')
+            .filter(|s| !s.is_empty())
+            .last()
+            .unwrap_or("video")
+            .to_string();
+        if output_name.contains('?') {
+            output_name = output_name.split('?').next().unwrap().to_string();
+        }
+        output_name.push_str(".mp4");
+
+        tokio::fs::write(&output_name, bytes)
+            .await
+            .map_err(|_| DownloaderError::IOError)?;
+
+        Ok(output_name)
     }
-    let _ = tab.close(false);
 
-    if !found {
-      return Err(DownloaderError::FetchError);
+    async fn get_variant_playlist(
+        _browser: &Browser,
+        _url: &str,
+    ) -> Result<VariantPlaylist, DownloaderError> {
+        //since we are downloading the .mp4 file directly
+        Err(DownloaderError::NoMasterPlaylistError)
     }
 
-    let client = reqwest::Client::new();
+    fn validate_url(url: &str) -> Result<(), DownloaderError> {
+        let tiktok_regex =
+            regex::Regex::new(r"https:\/\/(www\.)?tiktok.com\/@.+\/video\/\d+(\?.*)?").unwrap();
+        let tiktok_short_regex =
+            regex::Regex::new(r"https:\/\/(www\.)?\w+\.tiktok\.com\/[^@]\w+").unwrap();
 
-    let url_mutex_guard = intercepted_url.lock().await.to_owned();
-    let cookie_mutex_guard = intercepted_cookie.lock().await.to_owned();
-    let response = client
-      .get(url_mutex_guard)
-      .header("User-Agent", r"Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:133.0) Gecko/20100101 Firefox/133.0")
-      .header("Referer", r"https://www.tiktok.com/")
-      .header("Cookie", cookie_mutex_guard)
-      .send()
-      .await
-      .map_err(|_| DownloaderError::FetchError)?;
+        if !tiktok_regex.is_match(url) && !tiktok_short_regex.is_match(url) {
+            return Err(DownloaderError::UnsupportedPlatformError);
+        }
 
-    let bytes = response.bytes().await.map_err(|_| DownloaderError::FetchError)?;
-
-    let mut output_name = url.split('/').filter(|s| !s.is_empty()).last().unwrap_or("video").to_string();
-    if output_name.contains('?') {
-      output_name = output_name.split('?').next().unwrap().to_string();
+        Ok(())
     }
-    output_name.push_str(".mp4");
-
-    tokio::fs::write(&output_name, bytes).await.map_err(|_| DownloaderError::IOError)?;
-
-    Ok(output_name)
-  }
-
-  async fn get_variant_playlist(_browser: &Browser, _url: &str) -> Result<VariantPlaylist, DownloaderError> {
-    //since we are downloading the .mp4 file directly
-    Err(DownloaderError::NoMasterPlaylistError)
-  }
-
-  fn validate_url(url: &str) -> Result<(), DownloaderError> {
-    let tiktok_regex = regex::Regex::new(r"https:\/\/(www\.)?tiktok.com\/@.+\/video\/\d+(\?.*)?").unwrap();
-    let tiktok_short_regex = regex::Regex::new(r"https:\/\/(www\.)?\w+\.tiktok\.com\/[^@]\w+").unwrap();
-
-    if !tiktok_regex.is_match(url) && !tiktok_short_regex.is_match(url) {
-      return Err(DownloaderError::UnsupportedPlatformError);
-    }
-
-    Ok(())
-  }
 }
 
-fn get_interceptor(url: Arc<Mutex<String>>, cookie: Arc<Mutex<String>>) -> Arc<dyn RequestInterceptor + Send + Sync> {
-  Arc::new(move |_transport: Arc<Transport>, _session_id: SessionId, event: RequestPausedEvent| {
-    let request = event.params.request.clone();
+fn get_interceptor(
+    url: Arc<Mutex<String>>,
+    cookie: Arc<Mutex<String>>,
+) -> Arc<dyn RequestInterceptor + Send + Sync> {
+    Arc::new(
+        move |_transport: Arc<Transport>, _session_id: SessionId, event: RequestPausedEvent| {
+            let request = event.params.request.clone();
 
-    let mut url_mutex_guard = url.blocking_lock();
-    let mut cookie_mutex_guard = cookie.blocking_lock();
-    if request.url.contains("mime_type=video_mp4") && url_mutex_guard.is_empty() {
-      *url_mutex_guard = request.url;
+            let mut url_mutex_guard = url.blocking_lock();
+            let mut cookie_mutex_guard = cookie.blocking_lock();
+            if request.url.contains("mime_type=video_mp4") && url_mutex_guard.is_empty() {
+                *url_mutex_guard = request.url;
 
-      if let Some(header) = request.headers.0 {
-        if let Some(cookie_header) = header.get("Cookie") {
-          *cookie_mutex_guard = cookie_header.clone().take().to_string();
-        }
-      }
-    }
+                if let Some(header) = request.headers.0 {
+                    if let Some(cookie_header) = header.get("Cookie") {
+                        *cookie_mutex_guard = cookie_header.clone().take().to_string();
+                    }
+                }
+            }
 
-    RequestPausedDecision::Continue(None)
-  })
+            RequestPausedDecision::Continue(None)
+        },
+    )
 }
 
 fn get_request_patterns() -> Vec<RequestPattern> {
-  vec![
-    RequestPattern {
-    url_pattern: Some("https://v16-webapp-prime.tiktok.com/video/*".to_string()),
-    resource_Type: Some(ResourceType::Xhr),
-    request_stage: Some(RequestStage::Request),
-    },
-    RequestPattern {
-    url_pattern: Some("https://v16-webapp-prime.tiktok.com/video/*".to_string()),
-    resource_Type: Some(ResourceType::Media),
-    request_stage: Some(RequestStage::Request),
-    },
-  ]
+    vec![
+        RequestPattern {
+            url_pattern: Some("https://v16-webapp-prime.tiktok.com/video/*".to_string()),
+            resource_Type: Some(ResourceType::Xhr),
+            request_stage: Some(RequestStage::Request),
+        },
+        RequestPattern {
+            url_pattern: Some("https://v16-webapp-prime.tiktok.com/video/*".to_string()),
+            resource_Type: Some(ResourceType::Media),
+            request_stage: Some(RequestStage::Request),
+        },
+    ]
 }
 
 fn get_initial_tab_create_target() -> CreateTarget {
-  CreateTarget {
-    url: "about::blank".to_string(),
-    width: None,
-    height: None,
-    browser_context_id: None,
-    enable_begin_frame_control: None,
-    new_window: Some(true),
-    background: Some(true),
-  }
+    CreateTarget {
+        url: "about::blank".to_string(),
+        width: None,
+        height: None,
+        browser_context_id: None,
+        enable_begin_frame_control: None,
+        new_window: Some(true),
+        background: Some(true),
+        left: None,
+        top: None,
+        window_state: None,
+        for_tab: None,
+        hidden: None,
+    }
 }
